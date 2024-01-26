91d5f2d496bfb66aba2bdcfd4a73a9e4cbef67ae
ltphuc093 <lethanhphuc093@gmail.com>
2024-01-26 21:00:34 +0700

    test

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { join } = require('node:path');
 const { WebSocketServer } = require('ws');
 const app = express();
-const wss = new WebSocketServer({ port: 7071  });
-const port = process.env.PORT || 8080;
+const wss = new WebSocketServer({ port: 443  });
+const port = process.env.PORT || 8000;
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     console.log('received: %s', data);
